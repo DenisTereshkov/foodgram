a58069ded160b860d5a7c1872c6a0ddf730057b6
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-03-21 15:59:39 +0300

    repush

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,11 @@
 import base64
+
 from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
 from django.core.validators import (
     MaxValueValidator,
     MinValueValidator
 )
-from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
