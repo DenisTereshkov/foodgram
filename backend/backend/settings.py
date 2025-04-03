07d9ab41d019b1e500dfd6f5b38f7bb4875c3514
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-04-03 14:09:48 +0300

    repush

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,13 +79,13 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 AUTH_USER_MODEL = 'users.User'
